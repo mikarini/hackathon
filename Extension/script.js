523b010797a69a89b95af3d3f6278fa1fc5671cb
Francois <steve.gagne@hotmail.com>
2018-03-24 15:12:07 -0400

    Fixed Extension with JSON

--- Extension/script.js
+++ Extension/script.js
@@ -1,38 +1,17 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {  
-	displayResult(request.query);
+	parseJason();
     sendResponse();
 });
-document.onload = startSearch();
 
-function startSearch(){
-	linkCss();
-	var input = document.createElement("INPUT");
-	input.setAttribute("style", "display:block;");
-	
-}
-
-function linkCss()
-{
-	var head  = document.getElementsByTagName('head')[0];
-		var stylesheet  = document.createElement('link');
-		stylesheet.rel  = 'stylesheet';
-		stylesheet.type = 'text/css';
-		stylesheet.href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
-		head.appendChild(stylesheet);
-	
-}
-
-
-//JS from the file that I was working on.
-
+var jsonArray;
 var xml;
 
 function parseJason()
 {
 	xml = new XMLHttpRequest();
 	xml.onreadystatechange = getData;
-	xml.open("POST","http://13.72.105.141",true);
+	xml.open("GET","http://13.72.105.141/feed",true);
 	xml.send();
 }
 
@@ -40,46 +19,52 @@ function getData()
 {
 	if(xml.readyState == XMLHttpRequest.DONE && xml.status == 200)
 	{
-		console.log(JSON.parse(xml.responseText));
+		console.log(JSON.parse(xml.responseText)[0]);
+		jsonArray = JSON.parse(xml.responseText);
 		fillInCards();
 	}
 	else{
 		console.log("There was a problem with connection");
-		fillInCards();
 	}
 	
 }
-function displayResult(query)
-{
+
+function fillInCards()
+{	
+	
 	document.getElementById("container-anchor").innerHTML = "";
 	for (var j = 0; j < 2 ; j++)
 	{
+		if(j == 0)
+			var category = jsonArray[0].category;
+		else
+			var category = jsonArray[3].category;
 		var section = document.createElement("SECTION");
 		section.setAttribute("id","section");
 		document.getElementById("container-anchor").appendChild(section);
 		var heading = document.createElement("H3");
-		heading.setAttribute("style","color: white;");
-		heading.appendChild(document.createTextNode("Catégorie: Business"));
+		heading.appendChild(document.createTextNode(category));
 		section.appendChild(heading);
 		
 		var cardDeck = document.createElement("SECTION");
 		cardDeck.setAttribute("class", "card-deck");
-		cardDeck.setAttribute("title","Category: Commerce.");
+		cardDeck.setAttribute("title","Categorie : " + category);
 		section.appendChild(cardDeck);
 
 		
 		for(var i = 0; i < 3 ; i++)
 		{
+			var anArticle = jsonArray[i];
+			
 			var card = document.createElement("article");
 			card.setAttribute("class", "card");
-			card.setAttribute("style", "border: 1px solid black; padding: 5px; margin: 10px;");
-			card.setAttribute("id", "card");
+			card.setAttribute("id", anArticle.video_id);
 			cardDeck.appendChild(card);
 			
 			var img = document.createElement("IMG");
 			img.setAttribute("class","card-image-top");
-			img.setAttribute("src","https://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprodmigration%2Fweb%2Fbin%2F5ca5cbde-984c-328c-97f5-3805b28ebb87.jpg?crop=1500%2C1000%2C0%2C0&resize=685");
-			img.setAttribute("alt","Image title");
+			img.setAttribute("src",""+ anArticle.image.href);
+			img.setAttribute("alt","Image for " + anArticle.title);
 			card.appendChild(img);
 			
 			var cardBody = document.createElement("DIV");
@@ -88,12 +73,12 @@ function displayResult(query)
 			
 			var title = document.createElement("H5");
 			title.setAttribute("class","card-title");
-			title.appendChild(document.createTextNode(query));
+			title.appendChild(document.createTextNode(anArticle.title));
 			cardBody.appendChild(title);
 			
 			var description = document.createElement("P");
 			description.setAttribute("class","card-text");
-			description.appendChild(document.createTextNode("Some quick example text to build on the card title and make up the bulk of the card's content."));
+			description.appendChild(document.createTextNode(anArticle.description));
 			cardBody.appendChild(description);
 			
 			var btn = document.createElement("A");
